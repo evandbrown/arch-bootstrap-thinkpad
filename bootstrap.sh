70e5c9a6a0053e0bc2cae9316185d50e497551a2
Evan Brown <evandbrown@gmail.com>
2015-05-10 20:12:51 -0700

    Add curl | bash install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,6 @@
-#!/bin/bash
-# Install Puppet and librarian-puppet
-sudo pacman -S ruby puppet
-sudo gem install librarian-puppet
-
-# Configure librarian and install modules
-rm -rf /etc/puppet/modules
-cp Puppetfile /etc/puppet/
-cd /etc/puppet
-sudo ~/.gem/ruby/2.2.0/bin/librarian-puppet install
-
-# Run Puppet
-cd -
-sudo puppet apply site.pp
-
-# Set password for user
-echo "Set password for user"
-passwd evan
+#! /bin/bash
+pacman -S git
+cd $HOME
+git clone git@github.com:evandbrown/arch-bootstrap-thinkpad.git
+cd arch-bootstrap-thinkpad
+./install.sh
