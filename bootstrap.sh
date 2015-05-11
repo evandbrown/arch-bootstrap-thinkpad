9f06703f8aa3206b1dd91d9108a4a933e28f82c6
Evan Brown <evanbrown@google.com>
2015-05-11 13:07:50 -0700

    Allow group write to boostrap dir

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,6 @@
 pacman -S --noconfirm git openssh
 cd $HOME
 git clone https://github.com/evandbrown/arch-bootstrap-thinkpad.git
+chmod -R g+w arch-bootstrap-thinkpad
 cd arch-bootstrap-thinkpad
 ./install.sh
