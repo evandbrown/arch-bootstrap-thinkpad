3b3dc5424924398f0e615dcdb82426753f9d5ca0
Evan Brown <evandbrown@gmail.com>
2015-05-10 20:31:36 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 pacman -S --noconfirm git openssh
 cd $HOME
-git clone git@github.com:evandbrown/arch-bootstrap-thinkpad.git
+git clone https://github.com/evandbrown/arch-bootstrap-thinkpad.git
 cd arch-bootstrap-thinkpad
 ./install.sh
