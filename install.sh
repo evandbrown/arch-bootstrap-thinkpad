41967231c6ba814ab0a7a894898588b4396aef65
Evan Brown <evanbrown@google.com>
2015-05-11 19:44:26 -0700

    Remove sudo

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # Install Puppet and librarian-puppet
-sudo pacman -S --noconfirm ruby puppet
-sudo gem install librarian-puppet
+pacman -S --noconfirm ruby puppet
+gem install librarian-puppet
 
 # Configure librarian and install modules
 rm -rf /etc/puppet/modules
 cp Puppetfile /etc/puppet/
 cd /etc/puppet
-sudo ~/.gem/ruby/2.2.0/bin/librarian-puppet install
+/root/.gem/ruby/2.2.0/bin/librarian-puppet install
 
 # Allow group to write to puppet
 chmod -R g+w /etc/puppet
 
 # Run Puppet
 cd -
-sudo puppet apply site.pp
+puppet apply site.pp
 
 # Set password for user
 echo "Set password for user"
