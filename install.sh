3178c2855f61a700854d0db6c7e04b93efa1eb15
Evan Brown <evanbrown@google.com>
2015-05-11 19:41:32 -0700

    Add user to groups

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 sudo pacman -S --noconfirm ruby puppet
 sudo gem install librarian-puppet
 
-# Allow group to write to puppet
-chmod -R g+w /etc/puppet
-
 # Configure librarian and install modules
 rm -rf /etc/puppet/modules
 cp Puppetfile /etc/puppet/
 cd /etc/puppet
 sudo ~/.gem/ruby/2.2.0/bin/librarian-puppet install
+
+# Allow group to write to puppet
+chmod -R g+w /etc/puppet
 
 # Run Puppet
 cd -
--- site.pp
+++ site.pp
@@ -1,6 +1,7 @@
 node default {
   class {'archlinux_workstation':
     username => evan,
+    groups => ['root', 'wheel'],
     dotfiles => "https://github.com/evandbrown/dotfiles.git",
     gui      => i3,
   }
